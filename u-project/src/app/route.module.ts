000bf5deea4efffdb409422b0234e64d969f684c
Mohammad Al-Haimouni <m.alhaimouni@gmail.com>
2023-04-25 21:38:14 +0300

    all route changes

---
+++ u-project/src/app/route.module.ts
@@ -0,0 +1,30 @@
+import { RouterModule, Routes } from "@angular/router";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { NgModule } from "@angular/core";
+import { NotFoundComponent } from "./notFound/not-found.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeLoadingComponent } from "./recipes/recipe-loading/recipe-loading.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {
+    path: 'recipes', component: RecipesComponent, children: [
+      { path: '', component: RecipeLoadingComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent },
+    ]
+  },
+  { path: 'shopping-list', component: ShoppingListComponent, children: [] },
+  { path: 'not-found', component: NotFoundComponent, children: [] },
+  { path: '**', redirectTo: '/not-found' },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutes { }
