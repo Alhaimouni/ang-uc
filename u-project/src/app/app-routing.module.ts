ad2222d16c1bceee66db48c21c2902fe5ec9ef71
Mohammad Al-Haimouni <m.alhaimouni@gmail.com>
2023-05-20 21:08:28 +0300

    adding notfound Route

--- u-project/src/app/app-routing.module.ts
+++ u-project/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeResolver } from './recipes/recipe-resolver.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -19,6 +20,8 @@ const appRoutes: Routes = [
     ]
   },
   { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo:'/not-found'},
 ];
 
 @NgModule({
