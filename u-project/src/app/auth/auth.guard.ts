d9ac7ec6d409445c41e670443e88488b590122b8
Mohammad Al-Haimouni <m.alhaimouni@gmail.com>
2023-06-14 16:23:32 +0300

    adding my fb

--- u-project/src/app/auth/auth.guard.ts
+++ u-project/src/app/auth/auth.guard.ts
@@ -13,7 +13,7 @@ import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
